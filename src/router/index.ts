dec72068b1c283e4983d8d4e0cfd54b36dcff586
Curtis-CS <cburchfield@nevada.unr.edu>
2023-02-14 22:51:28 -0800

    Curtis Update About Us V3

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,12 +11,20 @@ const routes: Array<RouteConfig> = [
     component: Home
   },
   {
-    path: '/HowTo',
-    name: 'HowTo',
+    path: '/AboutUs',
+    name: 'AboutUs',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/HowToUse.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutUs.vue')
+  },
+  {
+    path: '/Contribute',
+    name: 'Contribute',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Contribute.vue')
   }
 ]
 
