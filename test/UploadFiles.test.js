112ede70b8a2b845c78a0efa2c5da731a89f4513
Michael <96790637+mmoosmuller@users.noreply.github.com>
2023-03-03 18:54:33 -0800

    Tests

--- test/UploadFiles.test.js
+++ test/UploadFiles.test.js
@@ -6,14 +6,16 @@ import { mount } from "@vue/test-utils"
 import UploadFiles from "@/components/UploadFiles.vue"
 import { describe, expect, test } from "vitest"
 
+// Author: Michael Moosmuller
 describe('method tests', () => {
     it('run_status is false by default', () => {
         const wrapper = mount(UploadFiles)
         expect(wrapper.vm.run_status).equals(false)
     })
 
-
 })
+
+// Author: Ian Ayers
 test("check file clear", async ()=> {
     expect(UploadFiles.methods.clearFiles()).equals(false)
 })
