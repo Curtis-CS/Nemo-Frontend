2f188faa11e6262d03ba80fd6c1e8a6cac0245fd
Ayers-Ian <Ianfire01@gmail.com>
2023-03-09 08:48:40 -0800

    Ian -HowTo- updating HowTo with more images and step-by-step directions *tempFin*

--- src/views/HowTo.vue
+++ src/views/HowTo.vue
@@ -4,28 +4,33 @@
         <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
           <div class="Padding">
             <h2 class="display-6 lh-1 mb-5">How To Use</h2>
-            <p class="lead fw-bold mb-5 mb-lg-0" style="padding-top: 5vw">
-              Step: 1
+            <p class="lead fw-bold mb-5 mb-lg-0" style="float: left; padding-bottom: 5vw">
+              Step: 1 - Uploading Data
             </p>
-            <div style="float: left; padding-left: 2vw;">
+            <div style="float: left; padding-top: 2vw; padding-bottom: 2vw;">
               <img src="/emptyUpload.png" class="Images">
             </div>
-            <p class="lead fw-normal text-muted mb-5 mb-lg-0" style=" padding-bottom: 10px">
-              Choose between uploading your own images and using a pre-determined set of images
+            <p class="lead fw-normal text-muted mb-5 mb-lg-0" style="float: left; padding-bottom: 10px">
+              Press "Choose Files" or drag and drop a file into the dropbox.
             </p>
-            <p class="lead fw-bold mb-5 mb-lg-0">
-              Step: 2
+            <p class="lead fw-bold mb-5 mb-lg-0" style="float: right; padding-top: 5vw; padding-bottom: 5vw">
+              Step: 2 - Running the Model
             </p>
-            <div style="float: right; padding-right: 2vw;">
+            <div style="float: right; padding-top: 7vw; padding-bottom: 2vw">
               <img src="/nemoUpload.png" class="Images">
             </div>
-            <p class="lead fw-normal text-muted mb-5 mb-lg-0" style=" padding-bottom: 10px">
-              If you uploaded your own images, press "Run Nemo". If you want to use a pre-determined set, press "Run Example"
+            <p class="lead fw-normal text-muted mb-5 mb-lg-0" style="float: right; padding-bottom: 5vw">
+              Once you've uploaded your image(s), you should see the files you selected in the drop box as well as a
+              "Run" button. Press this button once you have confirmed that the images you uploaded are what you want
+              to have run through the model.
             </p>
-            <p class="lead fw-bold mb-5 mb-lg-0">
-              Step: 3
+            <p class="lead fw-bold mb-5 mb-lg-0" style="float: left; padding-bottom: 10px">
+              Step: 3 - Get Results
             </p>
-            <p class="lead fw-normal text-muted mb-5 mb-lg-0" style=" padding-bottom: 10px">
+            <div style="float: left; padding-top: 2vw; padding-bottom: 2vw">
+              <img src="/nemoResults.png" class="Images">
+            </div>
+            <p class="lead fw-normal text-muted mb-5 mb-lg-0" style="float: left; padding-bottom: 10px; padding-top: 5px">
               Wait patiently for NEMOs response, and view the results!
             </p>
           </div>
@@ -44,7 +49,7 @@
   font-size: 1vw;
 }
 .Images {
-	width: 65vw;
+	width: 30vw;
 	height: auto;
 }
 </style>
