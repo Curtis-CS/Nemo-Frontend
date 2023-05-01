edce8b8fd04ff3ab9a2c3c102a7eb633edda53d4
Michael-CSE-UNR <96790637+Michael-CSE-UNR@users.noreply.github.com>
2023-05-01 14:42:47 -0700

    Update AboutUs.vue

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,59 +1,216 @@
 <template>
   <div class="AboutUs">
-  <section class="bg-white">
-    <div class="container px-5 mt-custom max-column-width min-column-width">
-      <div class="row gx-5 align-items-start justify-content-center justify-content-lg-between">
-        <h2 class="text-center lh-1 mt-6 mb-5">Nemo Interface: Wildfire Smoke Detection</h2>
-        <h5 class="fw-bold mb-lg-4 mt-4 text-center"> Team #15 </h5>
-        <p class="fw-normal text-muted text-center mb-lg-2">
-          Curtis Burchfield | Ian Ayers | Michael Moosmuller
-        </p>
-        <p class="fw-normal text-muted text-center mb-lg-4">
-          cburchfield@unr.edu | iayers@unr.edu | mmoosmuller@unr.edu
-        </p>
-        <p class="fw-normal text-muted text-center mb-lg-0">
-          CS 426 Senior Project <br>
-          Computer Science and Engineering (CSE) <br>
-          University of Nevada Reno (UNR) <br>
-          Spring 2023 <br>
-        </p>
+<!--    Title Section-->
+    <section class="bg-light custom-title">
+      <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-9">
+              <h2 class="display-6 text-center">Nemo Interface</h2>
+              <h2 class="display-6 text-center mt-3">Wildfire Smoke Detection</h2>
+            </div>
+        </div>
       </div>
-      <div class="row gx-5 align-items-start justify-content-center justify-content-lg-between mt-5">
-        <h5 class="fw-bold text-center lh-1 mb-4 mt-5 min-width"> Abstract </h5>
-        <p class="fw-normal text-muted text-center mb-5 mb-lg-0">
-          Deep-learning (DL) based object detection algorithms are beneficial to areas of climate science involving
-          wildfire prevention, observation, and intelligence. As wildfires become more prevalent, the need for
-          advanced techniques in processing video feeds has become crucial for smoke detection. Existing DL-based
-          techniques have been developed based on convolution networks and are proven to be effective in wildfire
-          detection. However, these datasets are primarily commercial and/or closed-sourced. One of the few open-source
-          models is Nemo, a benchmark for fine-grained wildfire smoke detection in the incipient stage of a wildfire.
-          Using this model as a basis, our team intends to collaborate with its authors to build a convenient user
-          interface and refined data preprocessing for accurately detecting wildfire smoke in adverse weather or
-          nighttime conditions.
-        </p>
+    </section>
+<!--    List Section-->
+    <section class="bg-white mb-5">
+<!--      Team-->
+      <div class="row">
+        <div class="col-lg-1 align-content-center"/>
+        <div class="col-lg-1 align-content-center mt-5">
+          <img src="../../public/list_icon.png" class="custom-circle"/>
+        </div>
+        <div class="col-12 col-lg-9 bg mt-5">
+          <div class="card bg-light border-white custom-min-width">
+            <h5 class="card-title text-center display-7 mt-3">Team 15</h5>
+            <div class="card-body mt-4">
+              <p class=" text-center">
+                CS426 - Senior Project - Spring 2023 <br>
+                Computer Science and Engineering (CSE) <br>
+                University of Nevada Reno (UNR)
+              </p>
+              <div class="row align-content-center mt-5">
+                <div class="col-12 col-lg-4 text-center">
+                  <h6 class="display-7">Curtis Burchfield</h6>
+                  <p>cburchfield@unr.edu</p>
+                </div>
+                <div class="col-12 col-lg-4 text-center">
+                  <h6 class="display-7">Ian Ayers</h6>
+                  <p>iayers@unr.edu</p>
+                </div>
+                <div class="col-12 col-lg-4 text-center">
+                  <h6 class="display-7">Michael Moosmuller</h6>
+                  <p>mmoosmuller@unr.edu</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-1 align-content-center"/>
+        </div>
       </div>
-      <div class="row gx-5 align-items-start justify-content-center justify-content-lg-between">
-        <h5 class="fw-bold text-center lh-1 mb-4 mt-5 min-width"> Instructors </h5>
-        <p class="fw-normal text-muted text-center mb-lg-4">
-          David Feil-Seifer | Devrin Lee
-        </p>
-        <h5 class="fw-bold text-center lh-1 mb-4 mt-5 min-width"> Project Manager </h5>
-        <p class="fw-normal text-muted text-center mb-lg-4">
-          Abhishek Kundu
-        </p>
-        <h5 class="fw-bold text-center lh-1 mb-4 mt-5 min-width"> Advisors </h5>
-        <p class="fw-normal text-center text-muted mb-5 mb-lg-4">
-          Dr.Lei Yang | Amirhesam Yazdi
-        </p>
-        <p class="fw-normal text-center text-muted mb-5 mb-lg-0">
-          Dr. Lei Yang: Associate Professor at UNR in CSE.
-        </p>
-        <p class="fw-normal text-center text-muted mb-5 mb-lg-0">
-          Amirhesam Yazdi: PhD graduate in CSE, advised by Dr. Feng Yan and Dr. Lei Yang
-        </p>
+<!--      Abstract-->
+      <div class="row">
+        <div class="col-lg-1 align-content-center"/>
+        <div class="col-lg-1 align-content-center mt-5">
+          <img src="../../public/list_icon.png" class="custom-circle" alt="Bullet point icon."/>
+        </div>
+        <div class="col-12 col-lg-9 mt-5 bg ">
+          <div class="card bg-light border-white custom-min-width">
+            <h5 class="card-title text-left display-7 mt-3 card-title-left-margin">Abstract</h5>
+            <div class="card-body text-justify">
+              &nbsp&nbsp&nbsp&nbsp Deep-learning (DL) based object detection algorithms are beneficial to areas of climate
+              science involving wildfire prevention, observation, and intelligence. As wildfires become
+              more prevalent, the need for advanced techniques in processing video feeds has become
+              crucial for smoke detection. Existing DL-based techniques have been developed based on
+              convolution networks and are proven to be effective in wildfire detection. However, these
+              datasets are primarily commercial and/or closed-sourced. One of the few open-source models
+              is Nemo, a benchmark for fine-grained wildfire smoke detection in the incipient stage of a
+              wildfire. Using this model as a basis, our team intends to collaborate with its authors to
+              build a convenient user friendly interface.
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-1 align-content-center"/>
       </div>
-    </div>
-  </section>
+<!--      Description-->
+      <div class="row">
+        <div class="col-lg-1 align-content-center"/>
+        <div class="col-lg-1 align-content-center mt-5">
+          <img src="../../public/list_icon.png" class="custom-circle" alt="Bullet point icon."/>
+        </div>
+        <div class="col-12 col-lg-9 mt-5 bg">
+          <div class="card bg-light border-white custom-min-width">
+            <h5 class="card-title text-left display-7 mt-3 card-title-left-margin">Description</h5>
+            <div class="card-body text-justify">
+              &nbsp&nbsp&nbsp&nbsp Our senior project plans to accomplish three main goals with integrating the Nemo smoke
+              detection model into a web application. The web application will help bring exposure to the
+              project through an attractive and simple to use interface, in the hopes of building
+              recognition of this model within the field of fire science. The user-friendly design is
+              built around the goal of making the model more accessible to users who may not be familiar
+              with computer science skills and technology. Finally, the model aims to provide a simple
+              way for users to contribute to the project by labeling and submitting images to help
+              improve the smoke detection model currently in place. The intended users of this web application range from ordinary people interested in
+              analyzing the risk of a wildfire in its incipient stage (based on images and videos) with
+              proximity to their businesses or properties, users who are interested in analyzing images
+              and videos of wildfires, emergency service members such as firefighters or forestry personnel
+              with the goal of rapidly analyzing the potential risk of a fire, and researchers in the field
+              who want to further expand or contribute their knowledge to building and improving the
+              current model and software.
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-1 align-content-center"/>
+      </div>
+<!--      Project Video-->
+      <div class="row">
+        <div class="col-lg-1 align-content-center"/>
+        <div class="col-lg-1 align-content-center mt-5">
+          <img src="../../public/list_icon.png" class="custom-circle" alt="Bullet point icon."/>
+        </div>
+        <div class="col-12 col-lg-9 mt-5 bg">
+          <div class="card bg-light border-white custom-min-width">
+            <h5 class="card-title text-left display-7 mt-3 card-title-left-margin">Project Video</h5>
+            <div class="card-body text-center">
+              <iframe class="mt-3" width="850" height="500" src="https://www.youtube.com/embed/_6SgI3MziSQ"/>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-1 align-content-center"/>
+      </div>
+<!--      Project Poster-->
+      <div class="row">
+        <div class="col-lg-1 align-content-center"/>
+        <div class="col-lg-1 align-content-center mt-5">
+          <img src="../../public/list_icon.png" class="custom-circle" alt="Bullet point icon."/>
+        </div>
+        <div class="col-12 col-lg-9 mt-5 bg">
+          <div class="card bg-light border-white custom-min-width">
+            <h5 class="card-title text-left display-7 mt-3 card-title-left-margin">Project Poster</h5>
+            <div class="card-body text-center">
+            <img src="public/POSTER_T15.png" class="mt-3 poster" alt="Image of our senior project poster."/>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-1 align-content-center"/>
+      </div>
+<!--      Advisors-->
+      <div class="row">
+        <div class="col-lg-1 align-content-center"/>
+        <div class="col-lg-1 align-content-center mt-5">
+          <img src="../../public/list_icon.png" class="custom-circle"/>
+        </div>
+        <div class="col-12 col-lg-9 bg mt-5">
+          <div class="card bg-light border-white custom-min-width">
+            <h5 class="card-title text-center display-7 mt-3">Advisors</h5>
+            <div class="card-body mt-4">
+              <div class="row align-content-center mt-3">
+                <div class="col-12 col-lg-1"/>
+                <div class="col-12 col-lg-5 text-center">
+                  <h6 class="display-7">Dr. Lei Yang</h6>
+                  <p>Associate Professor<br>CSE, UNR</p>
+                </div>
+                <div class="col-12 col-lg-5 text-center">
+                  <h6 class="display-7">Dr. Amirhessam Yazdi</h6>
+                  <p>Lecturer<br>CSE, UNR</p>
+                </div>
+                <div class="col-12 col-lg-1"/>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-1 align-content-center"/>
+        </div>
+      </div>
+<!--      Capstone Instructors-->
+      <div class="row">
+        <div class="col-lg-1 align-content-center"/>
+        <div class="col-lg-1 align-content-center mt-5">
+          <img src="../../public/list_icon.png" class="custom-circle"/>
+        </div>
+        <div class="col-12 col-lg-9 bg mt-5">
+          <div class="card bg-light border-white custom-min-width">
+            <h5 class="card-title text-center display-7 mt-3">Capstone Instructors</h5>
+            <div class="card-body mt-4">
+              <div class="row align-content-center mt-3">
+                <div class="col-12 col-lg-1"/>
+                <div class="col-12 col-lg-5 text-center">
+                  <h6 class="display-7">Dr. Dave Feil-Seifer</h6>
+                  <p>Associate Professor, Graduate Director<br>CSE, UNR</p>
+                </div>
+                <div class="col-12 col-lg-5 text-center">
+                  <h6 class="display-7">Devrin Lee</h6>
+                  <p>Instructor<br>CSE, UNR</p>
+                </div>
+                <div class="col-12 col-lg-1"/>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-1 align-content-center"/>
+        </div>
+      </div>
+<!--      Project Manager-->
+      <div class="row">
+        <div class="col-lg-1 align-content-center"/>
+        <div class="col-lg-1 align-content-center mt-5">
+          <img src="../../public/list_icon.png" class="custom-circle"/>
+        </div>
+        <div class="col-12 col-lg-9 bg mt-5">
+          <div class="card bg-light border-white custom-min-width">
+            <h5 class="card-title text-center display-7 mt-3">Project Manager</h5>
+            <div class="card-body mt-4">
+
+              <div class="row align-content-center mt-3">
+                <div class="col-12 col-lg-3"/>
+                <div class="col-12 col-lg-6 text-center">
+                  <h6 class="display-7">Abhishek Kundu</h6>
+                  <p>CSE, UNR</p>
+                </div>
+                <div class="col-12 col-lg-3"/>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-1 align-content-center"/>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -62,3 +219,31 @@ export default {
   name: "AboutUs"
 }
 </script>
+
+<style>
+
+.card-title-left-margin {
+  margin-left: 1rem;
+}
+
+.poster {
+  width: 850px;
+  height: 500px;
+}
+
+.custom-circle {
+  width: 4rem;
+  height: 4rem;
+}
+
+.custom-min-width {
+  width: 890px;
+}
+
+.custom-title {
+  padding-top: 6rem;
+  padding-bottom: 3rem;
+  background-color: #fafafa;
+}
+
+</style>
